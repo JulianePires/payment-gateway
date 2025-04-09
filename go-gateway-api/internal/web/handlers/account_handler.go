ce99fecd1c64f87a54b2d2d7af9a88b98d1c64d6
Juliane Pires <mpiresjuliane@outlook.com>
2025-04-09 13:26:16 -0300

    feat: create account server

---
+++ go-gateway-api/internal/web/handlers/account_handler.go
@@ -0,0 +1,57 @@
+package handlers
+
+import (
+	"encoding/json"
+	"github.com/devfullcycle/imersao22/go-gateway/internal/dto"
+	"github.com/devfullcycle/imersao22/go-gateway/internal/service"
+	"net/http"
+)
+
+type AccountHandler struct {
+	accountService *service.AccountService
+}
+
+func NewAccountHandler(accountService *service.AccountService) *AccountHandler {
+	return &AccountHandler{
+		accountService: accountService,
+	}
+}
+
+// Create cria uma nova conta
+func (h *AccountHandler) Create(w http.ResponseWriter, r *http.Request) {
+	var input dto.CreateAccountInput
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		http.Error(w, "Invalid input", http.StatusBadRequest)
+		return
+	}
+
+	output, err := h.accountService.CreateAccount(input)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(output)
+}
+
+// Get busca uma conta passando api_key no header
+func (h *AccountHandler) Get(w http.ResponseWriter, r *http.Request) {
+	apiKey := r.Header.Get("X-API-Key")
+	if apiKey == "" {
+		http.Error(w, "Missing API key", http.StatusUnauthorized)
+		return
+	}
+
+	output, err := h.accountService.FindByAPIKey(apiKey)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(output)
+}
