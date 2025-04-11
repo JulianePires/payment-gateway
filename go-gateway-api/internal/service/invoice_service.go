314c584c478081f16da87f5286df77af9e9adedf
Juliane Pires <mpiresjuliane@outlook.com>
2025-04-11 17:29:25 -0300

    feat: create invoice service

---
+++ go-gateway-api/internal/service/invoice_service.go
@@ -0,0 +1,103 @@
+package service
+
+import (
+	"github.com/devfullcycle/imersao22/go-gateway/internal/domain"
+	"github.com/devfullcycle/imersao22/go-gateway/internal/dto"
+)
+
+type InvoiceService struct {
+	invoiceRepository domain.InvoiceRepository
+	accountService    AccountService
+}
+
+func NewInvoiceService(invoiceRepository domain.InvoiceRepository, accountService AccountService) *InvoiceService {
+	return &InvoiceService{
+		invoiceRepository: invoiceRepository,
+		accountService:    accountService,
+	}
+}
+
+// Create cria uma nova fatura
+func (s *InvoiceService) Create(input *dto.CreateInvoiceInput) (*dto.InvoiceOutput, error) {
+	accountOutput, err := s.accountService.FindByAPIKey(input.APIKey)
+	if err != nil {
+		return nil, err
+	}
+
+	invoice, err := dto.ToInvoice(input, accountOutput.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := invoice.Process(); err != nil {
+		return nil, err
+	}
+
+	if invoice.Status == domain.StatusApproved {
+		if _, err := s.accountService.UpdateBalance(input.APIKey, invoice.Amount); err != nil {
+			return nil, err
+		}
+	}
+
+	if err := s.invoiceRepository.Save(invoice); err != nil {
+		return nil, err
+	}
+
+	output := dto.FromInvoice(invoice)
+	return output, nil
+}
+
+// GetByID busca uma fatura pelo ID
+func (s *InvoiceService) GetByID(id string, apiKey string) (*dto.InvoiceOutput, error) {
+	invoice, err := s.invoiceRepository.FindByID(id)
+	if err != nil {
+		return nil, err
+	}
+
+	accountOutput, err := s.accountService.FindByAPIKey(apiKey)
+	if err != nil {
+		return nil, err
+	}
+
+	if invoice.AccountID != accountOutput.ID {
+		return nil, domain.ErrUnauthorizedAccess
+	}
+
+	output := dto.FromInvoice(invoice)
+	return output, nil
+}
+
+// ListByAccountID busca faturas por ID da conta
+func (s *InvoiceService) ListByAccountID(accountID string) ([]*dto.InvoiceOutput, error) {
+	invoices, err := s.invoiceRepository.FindByAccountID(accountID)
+	if err != nil {
+		return nil, err
+	}
+
+	output := make([]*dto.InvoiceOutput, len(invoices))
+	for i, invoice := range invoices {
+		output[i] = dto.FromInvoice(invoice)
+	}
+
+	return output, nil
+}
+
+// ListByAccountAPIKey busca faturas por API key da conta
+func (s *InvoiceService) ListByAccountAPIKey(apiKey string) ([]*dto.InvoiceOutput, error) {
+	accountOutput, err := s.accountService.FindByAPIKey(apiKey)
+	if err != nil {
+		return nil, err
+	}
+
+	invoices, err := s.invoiceRepository.FindByAccountID(accountOutput.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	output := make([]*dto.InvoiceOutput, len(invoices))
+	for i, invoice := range invoices {
+		output[i] = dto.FromInvoice(invoice)
+	}
+
+	return output, nil
+}
