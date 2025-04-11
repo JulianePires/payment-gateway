dd9d221cb8b0cf4c888e015ea1c962c2b9af1177
Juliane Pires <mpiresjuliane@outlook.com>
2025-04-11 17:58:16 -0300

    feat: create auth middleware

---
+++ go-gateway-api/internal/web/middleware/auth_middleware.go
@@ -0,0 +1,42 @@
+package middleware
+
+import (
+	"errors"
+	"github.com/devfullcycle/imersao22/go-gateway/internal/domain"
+	"github.com/devfullcycle/imersao22/go-gateway/internal/service"
+	"net/http"
+)
+
+type AuthMiddleware struct {
+	accountService *service.AccountService
+}
+
+func NewAuthMiddleware(accountService *service.AccountService) *AuthMiddleware {
+	return &AuthMiddleware{
+		accountService: accountService,
+	}
+}
+
+// Authenticate verifica se o token de autenticação é válido
+func (m *AuthMiddleware) Authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		apiKey := r.Header.Get("X-API-Key")
+		if apiKey == "" {
+			http.Error(w, "Missing API key", http.StatusUnauthorized)
+			return
+		}
+
+		_, err := m.accountService.FindByAPIKey(apiKey)
+		if err != nil {
+			if errors.Is(err, domain.ErrAccountNotFound) {
+				http.Error(w, err.Error(), http.StatusUnauthorized)
+				return
+			}
+
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
